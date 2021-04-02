f1a1b09ac79b18fcd04a9168381b50fbdd6ce487
dev21102001 <Devkajal2001@gmail.com>
2021-04-01 19:27:17 -0700

    Utils to launch activities in MainActivity

--- app/src/main/java/com/example/virtualatm/MainActivity.java
+++ app/src/main/java/com/example/virtualatm/MainActivity.java
@@ -11,4 +11,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void widhdraw (View view) {
+        startActivity(new Intent(getApplicationContext(), WithdrawActivity.class));
+
+    }
+
+    public void addmoney (View view) {
+        startActivity(new Intent(getApplicationContext(), AddMoneyActivity.class));
+    }
+
+    public void statement (View view) {
+        startActivity(new Intent(getApplicationContext(), StatementActivity.class));
+
+    }
+
+    public void checkBalance (View view) {
+        startActivity(new Intent(getApplicationContext(), AccountBalanceActivity.class));
+
+    }
+
+    public void exit (View view) {
+        startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+        finish();
+    }
 }
