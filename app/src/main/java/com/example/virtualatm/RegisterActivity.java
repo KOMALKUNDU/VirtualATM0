ac79def5acb89ca7c8777b9f2d7c20f0d02760df
dev21102001 <Devkajal2001@gmail.com>
2021-04-01 19:29:40 -0700

    Initialize the views for registration activity

--- app/src/main/java/com/example/virtualatm/RegisterActivity.java
+++ app/src/main/java/com/example/virtualatm/RegisterActivity.java
@@ -2,14 +2,28 @@ package com.example.virtualatm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText name, email, phone, password;
+    Button Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        
+        name = findViewById(R.id.emailPhone);
+        email = findViewById(R.id.passWord);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
     }
     
     
