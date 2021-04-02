e1e086dde35ce92fcd1ed774da57ef988f7fa17c
KOMALKUNDU <Devkajal2001@gmail.com>
2021-04-01 19:45:26 -0700

    Fix errors

--- app/src/main/java/com/example/virtualatm/LogiActivity.java
+++ app/src/main/java/com/example/virtualatm/LogiActivity.java
@@ -2,7 +2,11 @@ package com.example.virtualatm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogiActivity extends AppCompatActivity {
 
