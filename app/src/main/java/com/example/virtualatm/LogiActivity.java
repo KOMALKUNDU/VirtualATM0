8da9fb0e2475ad2cacb3cfd207d85e7acb394347
KOMALKUNDU <komalkundu27@gmail.com>
2021-04-01 18:53:20 -0700

    Add LogiActivity

---
+++ app/src/main/java/com/example/virtualatm/LogiActivity.java
@@ -0,0 +1,14 @@
+package com.example.virtualatm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logi);
+    }
+}
